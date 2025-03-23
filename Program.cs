80c91fee7749a724343196257ae14697473982e8
aisyah <aaiisyah2007@gmail.com>
2025-03-24 05:41:56 +0800

    Menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_103022300116;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - AISYAH");
+        video.PrintVideoDetails();
+    }
+}
+
--- obj/Debug/net8.0/tpmodul6_103022300116.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul6_103022300116.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_103022300116")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67288c58236277af959f24e2a9a8b6c84148a166")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_103022300116")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_103022300116")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
